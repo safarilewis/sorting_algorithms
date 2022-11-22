425c30721ede13724fc2aae4cc841fdf07ce5f71
Lewis Safari <lleewwii1@gmail.com>
2022-11-22 09:54:25 +0300

    creating print files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * bubble_sort - Implements a bubble sort algorithm to in an array
+ * @array: Array to be sorted
+ * @size: Size of the Array to be sorted
+ * 
+ * Return: sorted array
+*/
+
+void bubble_sort(int *array, size_t size){
+    
+}
